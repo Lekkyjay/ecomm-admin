af521c1909458bf714a8eb9364814bc73e39f579
‘Lekky <lekkyjay@yahoo.com>
2023-07-06 19:43:50 +0200

    Added store-modal and modal-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
 import { Inter } from 'next/font/google'
+import { ModalProvider } from '@/providers/modal-provider'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,15 +10,14 @@ export const metadata = {
   description: 'Admin Dashboard',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
