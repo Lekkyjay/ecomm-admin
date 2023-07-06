1ee80bea0bea823940a9d98e251c345367c8cc97
‘Lekky <lekkyjay@yahoo.com>
2023-07-06 15:35:44 +0200

    Test dialog added to Page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
       <UserButton afterSignOutUrl="/" />
+      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   )
 }
