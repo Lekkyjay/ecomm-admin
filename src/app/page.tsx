eb3f89bd428faf256aa8d8d65c541adeb59e4530
‘Lekky <lekkyjay@yahoo.com>
2023-07-07 09:43:06 +0200

    Added navbar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client"
 
 import { useEffect } from "react";
-import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const onOpen = useStoreModal((state) => state.onOpen);
